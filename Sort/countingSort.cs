241d842666923000d55187e3d1ac4bb22857faa4
Milkis2022 <108617300+Milkis2022@users.noreply.github.com>
2022-12-28 23:58:38 +0900

    Update countingSort.cs

--- Sort/countingSort.cs
+++ Sort/countingSort.cs
@@ -1,1 +1,42 @@
+using System;
 
+class Test
+{
+    static void CountSort(char[] arr)
+    {
+        int n = arr.Length;
+        char[] output = new char[n];
+        int[] count = new int[256];
+
+        for (int i = 0; i < 256; ++i)
+            count[i] = 0;
+
+        for (int i = 0; i < n; ++i)
+            ++count[arr[i]];
+
+        for (int i = 1; i <= 255; ++i)
+            count[i] += count[i - 1];
+
+        for (int i = n - 1; i >= 0; i--)
+        {
+            output[count[arr[i]] - 1] = arr[i];
+            --count[arr[i]];
+        }
+
+        for (int i = 0; i < n; ++i)
+            arr[i] = output[i];
+    }
+
+    public static void Main()
+    {
+
+        char[] arr = { 'g', 'e', 'e', 'k', 's', 'f', 'o',
+                       'r', 'g', 'e', 'e', 'k', 's' };
+
+        CountSort(arr);
+
+        Console.Write("Sorted character array is ");
+        for (int i = 0; i < arr.Length; ++i)
+            Console.Write(arr[i] + " ");
+    }
+}
