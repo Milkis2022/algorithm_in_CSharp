4b6287c43267709c9a4d76d78d2580f2c7fef2d4
Milkis2022 <108617300+Milkis2022@users.noreply.github.com>
2023-01-21 23:47:29 +0900

    Update EuclideanAlgorithm.cs

--- Math/EuclideanAlgorithm.cs
+++ Math/EuclideanAlgorithm.cs
@@ -1,1 +1,30 @@
+using System;
 
+// Time Complexity: O(Log min(a, b))
+
+class Euclidean {
+    public static int gcd(int a, int b)
+    {
+        if (a == 0)
+            return b;
+ 
+        return gcd(b % a, a);
+    }
+ 
+    static public void Main()
+    {
+        int a = 10, b = 15, g;
+        g = gcd(a, b);
+        Console.WriteLine("GCD(" + a + " , " + b + ") = " + g);
+ 
+        a = 35;
+        b = 10;
+        g = gcd(a, b);
+        Console.WriteLine("GCD(" + a + " , " + b + ") = " + g);
+ 
+        a = 31;
+        b = 2;
+        g = gcd(a, b);
+        Console.WriteLine("GCD(" + a + " , " + b + ") = " + g);
+    }
+}
