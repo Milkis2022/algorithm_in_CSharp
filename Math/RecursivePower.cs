80c1e5b23e51bfdf62faf77bacc60c810299087a
Milkis2022 <108617300+Milkis2022@users.noreply.github.com>
2023-03-24 22:28:39 +0900

    Update RecursivePower.cs

--- Math/RecursivePower.cs
+++ Math/RecursivePower.cs
@@ -1,1 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace ConsoleApp1.algo
+{
+    class RecursivePower
+    {
+        public static void CalculatePower()
+        {
+
+        }
+    }
+}
